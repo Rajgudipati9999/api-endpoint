d01bf53b4fb691620a240855671ef07a89c9d71e
= <rahul.gudipati321@gmail.com>
2025-04-21 20:17:21 +0530

    all added

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
-const express = require('express')
-const app = express()
-const PORT = process.env.PORT || 3000
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 3000;
 
-app.use(express.json())
+app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('Welcome to the API!')
-})
+app.get("/", (req, res) => {
+  res.send("Welcome to the API!");
+});
 
 // Example route
-app.get('/api/users', (req, res) => {
+app.get("/api/users", (req, res) => {
   res.json([
-    { id: 1, name: 'Rahul' },
-    { id: 2, name: 'Gudipati' }
-  ])
-})
+    { id: 1, name: "Rahul" },
+    { id: 2, name: "Gudipati" },
+  ]);
+});
 
 app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`)
-})
+  console.log(`Server running on http://localhost:${PORT}`);
+});
